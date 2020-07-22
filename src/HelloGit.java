60e2cc3fa51b6e22751c7becaa5effab575f7956
unknown <muhsina.us@gmail.com>
2020-07-21 20:03:36 -0500

    updating child branch

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		System.out.println("My second print statment");
+		System.out.println(" Adding my child branch");
 
 	}
 
